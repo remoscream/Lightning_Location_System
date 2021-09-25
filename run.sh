5310b79ecf224aad0e34c4c7986b8c3f0adfdeeb
MadeinNagoya <88129217+madeinnagoya@users.noreply.github.com>
2021-09-26 05:23:22 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 tmux new-session -d -s AS3935 'python3 /home/pi/Lightning_Location_System/AS3935.py'
 
-tmux new-session -d -s BME280 'python3 /home/pi/Lightning_Location_System/BME280.py'
+tmux new-session -d -s BME280 'echo && python3 /home/pi/Lightning_Location_System/BME280.py'
 
-tmux new-session -d -s Condition 'python3 /home/pi/Lightning_Location_System/Condition.py'
+tmux new-session -d -s Condition 'echo && python3 /home/pi/Lightning_Location_System/Condition.py'
 
 tmux new-session -d -s Sync 'python3 /home/pi/Lightning_Location_System/VerMan_AS3935.py & python3 /home/pi/Lightning_Location_System/Sync.py'
