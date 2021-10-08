3a5d49beea946076a0187482cf875b63a701728d
remoscream <56284999+remoscream@users.noreply.github.com>
2021-10-08 16:13:01 +0900

    1. Use 'import CommonParameters as cp' to import common parameters 2. Delete function 'get_hardware_info', only get cpu temperature and save to data array directly (the size of array change from 12 to 9) 3. Delete code for saving to usb memory 4. Add some comments

--- Condition.py
+++ Condition.py
@@ -1,70 +1,53 @@
 from RPi_BME280 import RPi_BME280
 from Get_Time import get_time
+import CommonParameters as cp
 import time
 import numpy as np
 import psutil
 
-sensor = RPi_BME280(address=0x77, bus=1)
+# Initialization
+sensor = RPi_BME280(address=cp.add_condition, bus=cp.bus_condition)
 
 sensor.general_settings()
 sensor.get_calibration_param()
 
 
-# Get hardware information
-def get_hardware_info():
-    """ Get cpu temperature('C), cpu frequency(GHz), cpu load(%) and used memory(M) per second
-    """
-    hardware_info = np.zeros((1, 4))
-    # The temperature saved in dictionary 'cpu_thermal' --> list '0' --> tuple '0'
-    hardware_info[0, 0] = psutil.sensors_temperatures(fahrenheit=False)['cpu_thermal'][0][1]
-    hardware_info[0, 1] = psutil.cpu_freq(percpu=False).current / 1000  # GHz
-    hardware_info[0, 2] = psutil.cpu_percent(interval=0, percpu=False)  #
-    hardware_info[0, 2] = psutil.cpu_percent(interval=0, percpu=False)  # Interval should be set to 0 for a 0s delay
-    hardware_info[0, 3] = psutil.virtual_memory().used / 1000 / 1000  # Mega
-
-    return hardware_info
-
-
 if __name__ == '__main__':
     try:
-        hour_step = 1  # unit : hour
-        second_step = 1  # unit : second
-
-        data_num = int(hour_step * 60 * (60 / second_step))
-        dataset = np.zeros((data_num, 12))
+        data_num = int(cp.SyncPeriod / cp.TimeStep)
+        dataset = np.zeros((data_num, 9))
 
         # Main loop for sampling
         while True:
             time_now_text_terminal, time_now_text_file, time_now_int = get_time()
             # Sub loop for creating each data file
             for i in range(0, data_num, 1):
+                # Create time text
                 time_now_text_terminal, time_now_text_file, time_now_int = get_time()
                 dataset[i, 0:6] = time_now_int[0, 0:6]
 
-                temperature_real, _, humidity_real = sensor.read_data()
-                hardware_info = get_hardware_info()
+                # Read data from sensor (box temperature, box humidity, cpu temperature of raspi)
+                temperature, _, humidity = sensor.read_data()
+                cputemp = psutil.sensors_temperatures(fahrenheit=False)['cpu_thermal'][0][1]
 
-                dataset[i, 6] = temperature_real
-                dataset[i, 7] = humidity_real
+                # Save data to array
+                dataset[i, 6] = temperature
+                dataset[i, 7] = humidity
+                dataset[i, 8] = cputemp
 
-                dataset[i, 8:12] = hardware_info
-
+                # Print data in terminal
                 print('[Box condition] : ' + time_now_text_terminal)
-                print("Temperature : %6.2f C" % temperature_real)
-                print("Humidity : %6.2f %%\n" % humidity_real)
+                print("Temperature : %6.2f °C" % temperature)
+                print("Humidity : %6.2f %%\n" % humidity)
+                print("CPUtemp: %.2f °C\033[4A" % cputemp)
 
-                print('[Hardware info] : ')
-                print("CPUtemp: %.2f C, Freq: %.1f GHz, CPUload: %.1f %%, MemoryUsed: %.1f MB\033[0K\033[6A" % (
-                    dataset[i, 8], dataset[i, 9], dataset[i, 10], dataset[i, 11]))
+                time.sleep(cp.TimeStep)
 
-                time.sleep(second_step)
-
-            csv_filename = 'data_Condition_' + time_now_text_file + '.csv'
-            file_address_local = '/home/pi/Lightning_Location_System/data_Condition/'
-            file_address_usb = '/home/pi/Ustick/data_Condition/'
+            # Save data to csv file
+            csv_filename = 'data_condition_' + time_now_text_file + '.csv'
+            file_address_local = cp.FileAddress_condition
 
             np.savetxt(file_address_local + csv_filename, dataset, delimiter=',', fmt='%g')
-            np.savetxt(file_address_usb + csv_filename, dataset, delimiter=',', fmt='%g')
 
     except KeyboardInterrupt:
         print('\033[0J\n' + 'Monitoring process broken by user...')
