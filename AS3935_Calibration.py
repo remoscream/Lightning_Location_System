74690d92e25bb13709d147afbeffd3c021059998
remoscream <56284999+remoscream@users.noreply.github.com>
2021-10-08 17:06:30 +0900

    1. Renamed file from 'TunningAntenna,py' to 'AS3935_Calibration.py' 2. Use 'import CommonParameters as cp' to load common parameters 3. Change/add some comments

--- AS3935_Calibration.py
+++ AS3935_Calibration.py
@@ -1,41 +1,42 @@
-# Program for tunning antenna
+"""
+Program for tuning the antenna of AS3935
+"""
 
 from RPi_AS3935 import RPi_AS3935
-
 import RPi.GPIO as GPIO
+import CommonParameters as cp
 import time
-from datetime import datetime
 
 GPIO.setmode(GPIO.BCM)
-IRQ_GPIO = 4
-GPIO.setup(IRQ_GPIO, GPIO.IN)
+GPIO.setup(cp.IRQ_GPIONUM, GPIO.IN)  # Set GPIO mode as input
 
-sensor = RPi_AS3935(address=0x03, bus=1)
+sensor = RPi_AS3935(address=cp.add_ad3935, bus=cp.bus_as3935)
 
+# Set mode and noise floor for calibrating
 sensor.reset()
 print('set indoors')
 sensor.set_indoors(True)
 print("set noise floor to 0")
 sensor.set_noise_floor(0)
 
+
 sensor.read_data()
 print("register0x08= ", bin(sensor.registers[0x08]))
-# レジスタ0x08をアンテナ共振周波数をIRQピンに出力するよう指定
+
+# Set IRQ pin for output the waveform resonate signal
 sensor.set_disp_lco(True)
 sensor.read_data()
 print("register0x08= ", bin(sensor.registers[0x08]))
 
-# IRQピンにマルチメータを接続し周波数を測定せよ
+# User should use oscilloscope or other meters to measure the resonate frequency of the signal from IRQ
 print("Please measure the frequency of the IRQ pin by a multi-meter")
-# tune capを10秒ごとに変化させる
 print("tune cap is changed every 10 seconds")
 for x in range(0, 16):
     print("tun_cap= ", x)
     sensor.calibrate(tun_cap=x)
     time.sleep(10.0)
 
-time.sleep(5.0)
-# レジスタ0x08をもとに戻しておく
+# Return to the default value
 sensor.set_disp_lco(False)
 sensor.read_data()
 print("register0x08= ", bin(sensor.registers[0x08]))
