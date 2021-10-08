28b6b7e8fdb3f47944d37d492e5a74f4a7129e55
remoscream <56284999+remoscream@users.noreply.github.com>
2021-10-08 15:53:10 +0900

    1. Add 'import CommonParameters as cp' for loading command parameters 2. Change name of parameters T,P,H 3. Add '\033[4A' after humidity text to hold display position 4. Add/change some comments

--- BME280.py
+++ BME280.py
@@ -1,9 +1,11 @@
 from RPi_BME280 import RPi_BME280
 from Get_Time import get_time
+import CommonParameters as cp
 import time
 import numpy as np
 
-sensor = RPi_BME280(address=0x76, bus=1)
+# Initialization
+sensor = RPi_BME280(address=cp.add_bme280, bus=cp.bus_bme280)
 
 sensor.general_settings()
 sensor.get_calibration_param()
@@ -11,10 +13,7 @@ sensor.get_calibration_param()
 
 if __name__ == '__main__':
     try:
-        hour_step = 1  # unit : hour
-        second_step = 1  # unit : second
-
-        data_num = int(hour_step * 60 * (60 / second_step))
+        data_num = int(cp.SyncPeriod / cp.TimeStep)
         dataset = np.zeros((data_num, 9))
 
         # Main loop for sampling
@@ -22,23 +21,28 @@ if __name__ == '__main__':
             time_now_text_terminal, time_now_text_file, time_now_int = get_time()
             # Sub loop for creating each data file
             for i in range(0, data_num, 1):
+                # Create time text
                 time_now_text_terminal, time_now_text_file, time_now_int = get_time()
                 dataset[i, 0:6] = time_now_int[0, 0:6]
 
-                temperature_real, pressure_real, humidity_real = sensor.read_data()
+                # Read data from sensor
+                temperature, pressure, humidity = sensor.read_data()
 
-                dataset[i, 6] = temperature_real
-                dataset[i, 7] = pressure_real / 100
-                dataset[i, 8] = humidity_real
+                # Save data to array
+                dataset[i, 6] = temperature
+                dataset[i, 7] = pressure / 100
+                dataset[i, 8] = humidity
 
+                # Print data in terminal
                 print('[Weather now] : ' + time_now_text_terminal)
-                print("Temperature : %6.2f C" % temperature_real)
-                print('Pressure : %7.2f hPa' % (pressure_real / 100))
-                print("Humidity : %6.2f %%\033[4A" % humidity_real)
-                time.sleep(second_step)
+                print("Temperature : %6.2f °C" % temperature)
+                print('Pressure : %7.2f hPa' % (pressure / 100))
+                print("Humidity : %6.2f %%\033[4A" % humidity)  # '\033[4A' for display data at first position
+                time.sleep(cp.TimeStep)
 
+            # Save data to csv file
             csv_filename = 'data_BME280_' + time_now_text_file + '.csv'
-            file_address_local = '/home/pi/Lightning_Location_System/data_BME280/'
+            file_address_local = cp.FileAddress_bme280
 
             np.savetxt(file_address_local + csv_filename, dataset, delimiter=',', fmt='%g')
 
