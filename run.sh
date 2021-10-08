a1db02499207f78904794553e7055033b58152e3
remoscream <56284999+remoscream@users.noreply.github.com>
2021-10-08 12:45:54 +0900

    Change session names to lowercase

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-tmux new-session -d -s AS3935 'python3 /home/pi/Lightning_Location_System/AS3935.py'
+tmux new-session -d -s as3935 'python3 /home/pi/Lightning_Location_System/AS3935.py'
 
-tmux new-session -d -s BME280 'echo && python3 /home/pi/Lightning_Location_System/BME280.py'
+tmux new-session -d -s bme280 'echo && python3 /home/pi/Lightning_Location_System/BME280.py'
 
-tmux new-session -d -s Condition 'echo && python3 /home/pi/Lightning_Location_System/Condition.py'
+tmux new-session -d -s condition 'echo && python3 /home/pi/Lightning_Location_System/Condition.py'
 
-tmux new-session -d -s Sync 'python3 /home/pi/Lightning_Location_System/VerMan_AS3935.py & python3 /home/pi/Lightning_Location_System/Sync.py'
+tmux new-session -d -s sync 'python3 /home/pi/Lightning_Location_System/VerMan_AS3935.py & python3 /home/pi/Lightning_Location_System/Sync.py'
