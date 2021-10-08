0c5c22498413147ff28255971f8a5eddffcf950d
remoscream <56284999+remoscream@users.noreply.github.com>
2021-10-08 14:16:32 +0900

    1. Add 'import CommonParameters as cp' for loading common parameters. The old value of parameters of AS3935 are replaced by the parameters defined in 'CommonParameters.py' 2. File address is replaced by 'cp.FileAddress_as3935' 3. Change some comments

--- AS3935.py
+++ AS3935.py
@@ -1,24 +1,23 @@
 from RPi_AS3935 import RPi_AS3935
 from Get_Time import get_time
+import CommonParameters as cp
 import RPi.GPIO as GPIO
 import time
 import numpy as np
 
 GPIO.setmode(GPIO.BCM)
 
-# Common conditions
-IRQ_GPIO = 4
-
-sensor = RPi_AS3935(address=0x00, bus=1)  # Initialization
+# Initialization
+sensor = RPi_AS3935(address=cp.add_ad3935, bus=cp.bus_as3935)
+sensor.calibrate(tun_cap=cp.calibration)  # Adjust tuning capacitor
 sensor.reset()
-sensor.set_indoors(False)
-sensor.set_noise_floor(1)
-sensor.set_WDTH(1)
-sensor.set_SREJ(1)
-sensor.calibrate(tun_cap=0x06)  # Adjust tuning capacitor
+sensor.set_indoors(cp.indoor_mode)
+sensor.set_noise_floor(cp.noise_floor)
+sensor.set_WDTH(cp.WDTH)
+sensor.set_SREJ(cp.SREJ)
 
 
-# Judge the signal interrupt the IRQ
+# Judge the signal to interrupt the IRQ
 def judge_signal(self):
     time.sleep(0.003)
     global sensor
@@ -49,8 +48,8 @@ def judge_signal(self):
 
     dataset = np.concatenate((dataset, current_data), axis=0)
 
-    file_address = '/home/pi/Lightning_Location_System/data_AS3935/'
-    csv_filename = 'data_AS3935.csv'
+    file_address = cp.FileAddress_as3935
+    csv_filename = 'data_as3935.csv'
     np.savetxt(file_address + csv_filename, dataset, delimiter=',', fmt='%g')
 
 
@@ -58,14 +57,14 @@ def judge_signal(self):
 dataset = np.zeros([1, 9])  # Initialize data array
 
 # Create data file for test the cloud service
-file_address = '/home/pi/Lightning_Location_System/data_AS3935/'
-csv_filename = 'data_AS3935.csv'
+file_address = cp.FileAddress_as3935
+csv_filename = 'data_as3935.csv'
 np.savetxt(file_address + csv_filename, dataset, delimiter=',', fmt='%g')
 
-GPIO.setup(IRQ_GPIO, GPIO.IN)  # Set GPIO mode as input
-GPIO.add_event_detect(IRQ_GPIO, GPIO.RISING, callback=judge_signal)  
-# If detect signal, judge the type
+GPIO.setup(cp.IRQ_GPIONUM, GPIO.IN)  # Set GPIO mode as input
+GPIO.add_event_detect(cp.IRQ_GPIONUM, GPIO.RISING, callback=judge_signal) # If detect signal, judge the type
 
+# Print first message
 time_now_text_terminal, time_now_text_file, time_now_int = get_time()
 print(time_now_text_terminal, "Waiting for lightning...")
 
@@ -76,5 +75,5 @@ try:
 
 except KeyboardInterrupt:
     print('Detecting process broken by user...')
-    GPIO.remove_event_detect(IRQ_GPIO)
+    GPIO.remove_event_detect(cp.IRQ_GPIONUM)
     GPIO.cleanup()
