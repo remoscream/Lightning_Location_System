15a3a70d654bac462c86fbbe2d7e60888f9a1a7f
remoscream <56284999+remoscream@users.noreply.github.com>
2021-10-15 11:51:43 +0900

    Use full path of 'tmux'

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 git -C ~/Lightning_Location_System/ pull origin main
 
-tmux new-session -d -s as3935 'python3 /home/pi/Lightning_Location_System/AS3935.py'
+/usr/bin/tmux new-session -d -s as3935 'python3 /home/pi/Lightning_Location_System/AS3935.py'
 
-tmux new-session -d -s bme280 'echo && python3 /home/pi/Lightning_Location_System/BME280.py'
+/usr/bin/tmux new-session -d -s bme280 'echo && python3 /home/pi/Lightning_Location_System/BME280.py'
 
-tmux new-session -d -s condition 'echo && python3 /home/pi/Lightning_Location_System/Condition.py'
+/usr/bin/tmux new-session -d -s condition 'echo && python3 /home/pi/Lightning_Location_System/Condition.py'
 
-tmux new-session -d -s sync 'python3 /home/pi/Lightning_Location_System/AS3935_DataVerMan.py & python3 /home/pi/Lightning_Location_System/Sync.py'
+/usr/bin/tmux new-session -d -s sync 'python3 /home/pi/Lightning_Location_System/AS3935_DataVerMan.py & python3 /home/pi/Lightning_Location_System/Sync.py'
+
