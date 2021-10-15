743584b1e4c2f83aad44efb630dd93b602d13887
remoscream <56284999+remoscream@users.noreply.github.com>
2021-10-15 12:27:38 +0900

    First commit of files for setting startup running of run.sh

---
+++ SetStartupRunning/SetStartupRunning.sh
@@ -0,0 +1,10 @@
+# Shell script for set up boot-up running of run.sh
+# This script only run the commands from step 3 to 5 (README.md)
+# You can copy RunLLS.service in the folder to /lib/systemd/system/ the run this script
+
+$ sudo chmod 744 /home/pi/Lightning_Location_System/run.sh
+$ sudo chmod 664 /lib/systemd/system/RunLLS.service
+$ sudo systemctl daemon-reload
+$ sudo systemctl enable systemd-networkd
+$ sudo systemctl enable RunLLS.service
+$ sudo reboot
